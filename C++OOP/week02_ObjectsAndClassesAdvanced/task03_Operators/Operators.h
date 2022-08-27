e2605b4bfb70c629099ad46378f7a85aa28f2aff
AleksManolow <amanolov7@gmail.com>
2022-08-27 12:47:22 +0300

    Add task03

---
+++ C++OOP/week02_ObjectsAndClassesAdvanced/task03_Operators/Operators.h
@@ -0,0 +1,34 @@
+#ifndef __OPERATORS_H
+#define __OPERATORS_H
+
+#include <ostream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+std::ostream& operator<<(std::ostream& out, const std::vector<std::string>& v)
+{
+    for (std::string s : v)
+    {
+        out << s << std::endl;
+    }
+    return out;
+}
+
+std::vector<std::string>& operator<<(std::vector<std::string>& v, const std::string& s)
+{
+    v.push_back(s);
+    return v;
+}
+
+std::string operator+(std::string s, int i)
+{
+	std::ostringstream result;
+
+	result << s << i;
+
+	return result.str();  
+}
+
+
+#endif
